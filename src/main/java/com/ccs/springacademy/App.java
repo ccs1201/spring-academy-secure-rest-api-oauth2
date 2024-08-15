fc9f7e1f6a797c73faebcfed9b15fe9dce672902
Cleber Constante de Souza - CSZ <cleber.souza@iteris.com.br>
2024-08-15 08:18:54 -0300

    Initial commit

---
+++ src/main/java/com/ccs/springacademy/App.java
@@ -0,0 +1,13 @@
+package com.ccs.springacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAcademySecureRestApiOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAcademySecureRestApiOauth2Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/ccs/springacademy/SecurityFilterChainBean.java
@@ -0,0 +1,4 @@
+package com.ccs.springacademy;
+
+public class SecurityFilterChainBean {
+}
